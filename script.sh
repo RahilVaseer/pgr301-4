11e66430b2b4dc3a64c72962ae1872d101ce1ed4
RahilVaseer <rahil.vaseer@gmail.com>
2020-10-05 13:33:52 +0200

    fixed syntax error in script.sh

--- script.sh
+++ script.sh
@@ -1,5 +1,5 @@
     #!/bin/bash
     echo "$DOCKER_PASSWORD" | docker login -u "$DOCKER_USERNAME" --password-stdin
-    docker build . --tag pgr301 --build-arg JAR_FILE=./target/demo
+    docker build . --tag pgr301 --build-arg JAR_FILE=./target/demo.jar
     docker tag  pgr301  rahilvaseer/devops2020
     docker push rahilvaseer/devops2020
